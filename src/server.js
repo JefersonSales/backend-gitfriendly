5251785148d9b7e1228571307f6fff6f4d2b7e03
Jeferson Sales <jefersonsales1@gmail.com>
2019-10-23 17:05:14 -0300

    criando a estrutura do match

--- src/server.js
+++ src/server.js
@@ -2,13 +2,30 @@ const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
 
-const server = express()
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+	const { user } = socket.handshake.query
+	console.log(user, socket.id)
+	connectedUsers[user] = socket.id
+})
+
 mongoose.connect('mongodb+srv://gitfriendly:t0v9f4r2@tindev-rdfpr.mongodb.net/test?retryWrites=true&w=majority', {
 	useUnifiedTopology: true,
 	useNewUrlParser: true,
 })
 
-server.use(express.json())
-server.use(routes)
+app.use((req, res, next) => {
+	req.io = io
+	req.connectedUsers = connectedUsers
+	return next()
+})
+
+app.use(express.json())
+app.use(routes)
 
 server.listen(process.env.PORT || 3333)
